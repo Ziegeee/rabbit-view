927f267ceab4da6817493f6c9e3403400f5077ba
proud_ziege <121289423+Ziegeee@users.noreply.github.com>
2026-01-13 18:54:49 +0100

    Create +page.svelte

---
+++ src/routes/[rabbit]/+page.svelte
@@ -0,0 +1,7 @@
+<script>  
+    import RabbitForm from '$lib/components/RabbitForm.svelte';
+    import {page} from '$app/stores';
+</script>
+
+
+<RabbitForm rabbitId={$page.params.rabbit}></RabbitForm>
